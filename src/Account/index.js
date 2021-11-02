430f60c2b84e0f4edc4b9659b2894d9ff3816ba5
abdelmanaf <ourotoumanaf6@gmail.com>
2021-11-02 07:08:15 -0400

    add className of zero to the div

--- src/Account/index.js
+++ src/Account/index.js
@@ -2,17 +2,38 @@ import React, { Component } from 'react';
 
 
 class Account extends Component {
+
     state = {
       balance: 0
     }
-  render() {
-    return (
+    inputRef = React.createRef()
+
+    addClick = () => {
+      this.setState({ 
+        balance: parseInt(this.inputRef.current.value) + this.state.balance
+      })
+      this.inputRef.current.value= ''
+    }
+
+    subClick = () => {
+      let currentBalance = this.state.balance;
+      let inputValue = parseInt(this.inputRef.current.value);
+      if (currentBalance < inputValue) return;
+      let newBalance = currentBalance - inputValue;
+      this.setState({balance: newBalance})
+      this.inputRef.current.value = "";
+    }
+
+    render(){
+      return (
       <div className="account">
         <h2>{this.props.title}</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+
+        <div className={"balance" + (this.state.balance === 0 ? " zero" : "")}>$ {this.state.balance}</div>
+        
+        <input ref={this.inputRef} type="text" placeholder="enter an amount" />
+        <input onClick={this.addClick} type="button" value="Deposit" />
+        <input onClick={this.subClick} type="button" value="Withdraw" />
       </div>
     )
   }
